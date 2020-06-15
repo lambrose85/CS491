70e1a83f174617bc0e6f2d3655c1623897341a80
Lucas Ambrose <lucasambrose@siu.edu>
2020-06-15 12:06:45 -0500

    working on books function

--- hw1.py
+++ hw1.py
@@ -1,3 +1,18 @@
 """
 Book problem programming assignment 
 """
+
+
+def books(arr):
+    """
+    get names to add to book list from user
+    """
+    print('Add names to list, press q/Q to finish')
+    flag = True
+    while flag
+
+
+
+test= "hello"
+
+books(test)
