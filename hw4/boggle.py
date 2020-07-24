e208ad8379a8b225c7da83132659ba2ee6401a28
Lucas Ambrose <lucasambrose@siu.edu>
2020-07-23 20:27:04 -0500

    added user input to list

--- hw4/boggle.py
+++ hw4/boggle.py
@@ -3,7 +3,6 @@
 import random
 import itertools
 class Dice:
-
     myDice = [['A','E','A', 'N', 'E', 'G'], ['A', 'H', 'S', 'P', 'C', 'O'], ['A','S','P', 'F', 'F', 'K'], ['O','B','J', 'O', 'A', 'B'], ['I','O','T', 'M', 'U', 'C'], ['R','Y','V', 'D', 'E', 'L']
                 , ['L','R','E', 'I', 'X', 'D'], ['E','I','U', 'N', 'E', 'S'], ['W','N','G', 'E', 'E', 'H'], ['L','N','H', 'N', 'R', 'Z'], ['T','S','T', 'I', 'Y', 'D'], ['O','W','T', 'O', 'A', 'T']
                 , ['E','R','T', 'T', 'Y', 'L'], ['T','O','E', 'S', 'S', 'I'], ['T','E','R', 'W', 'H', 'V'], ['N','U','I', 'H', 'M', 'Qu']]
@@ -19,10 +18,20 @@ def printDice(x):
     for i in x:
         if counter%4 == 0:
             print("\n")
-        print(" "+i+" ", end='')
+        print(" ["+i+"] ", end='')
         counter += 1
     print("\n")   
-        
+
+userWords = []
 bog = Dice()
 test = bog.ran(bog.myDice)
 printDice(test)
+
+print("Start typing your words! (press enter after each word and enter 'X' when done): ")
+temp = True
+while temp == True:
+    word = input()
+    if word=="x" or word=="X":
+        temp = False
+    userWords.append(word)
+print(userWords)
